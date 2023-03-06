d826f1c6c548b48f1bd7d76631ff37c440be6221
Trevor Royer <troyer@redhat.com>
2023-03-06 08:40:07 -0600

    Add example for connecting to the kubeflow API

---
+++ pipelines/test_connection.py
@@ -0,0 +1,13 @@
+import kfp
+import urllib
+
+KUBEFLOW_ENDPOINT = (
+    "https://ds-pipeline-ui-odh-test.apps.cluster-mh667.mh667.sandbox2373.opentlc.com/"
+)
+BEARER_TOKEN = "sha256~"
+
+client = kfp.Client(
+    host=urllib.parse.urljoin(KUBEFLOW_ENDPOINT, "/pipeline"),
+    existing_token=BEARER_TOKEN,
+)
+print(client.list_experiments())
