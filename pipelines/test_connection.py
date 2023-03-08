12473e503bc99a57fe66a8fd0f04a987dda9543b
Trevor Royer <troyer@redhat.com>
2023-03-08 16:30:22 -0600

    add token command

--- pipelines/test_connection.py
+++ pipelines/test_connection.py
@@ -4,7 +4,7 @@ import urllib
 KUBEFLOW_ENDPOINT = (
     "https://ds-pipeline-ui-odh-test.apps.cluster-mh667.mh667.sandbox2373.opentlc.com/"
 )
-BEARER_TOKEN = "sha256~"
+BEARER_TOKEN = "sha256~"  # oc whoami --show-token
 
 client = kfp.Client(
     host=urllib.parse.urljoin(KUBEFLOW_ENDPOINT, "/pipeline"),
