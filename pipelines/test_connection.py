1505f4edb69eef22ee6d5b9f7e716a5142cca791
Trevor Royer <troyer@redhat.com>
2023-03-14 10:51:58 -0500

    update to dotenv

--- pipelines/test_connection.py
+++ pipelines/test_connection.py
@@ -1,13 +1,19 @@
-import kfp
+import os
+
 import urllib
 
-KUBEFLOW_ENDPOINT = (
-    "https://ds-pipeline-ui-odh-test.apps.cluster-mh667.mh667.sandbox2373.opentlc.com/"
-)
-BEARER_TOKEN = "sha256~"  # oc whoami --show-token
+import kfp
 
-client = kfp.Client(
-    host=urllib.parse.urljoin(KUBEFLOW_ENDPOINT, "/pipeline"),
-    existing_token=BEARER_TOKEN,
-)
-print(client.list_experiments())
+from dotenv import load_dotenv
+
+load_dotenv()
+
+kubeflow_endpoint = os.environ["KUBEFLOW_ENDPOINT"]
+bearer_token = os.environ["BEARER_TOKEN"]
+
+if __name__ == "__main__":
+    client = kfp.Client(
+        host=urllib.parse.urljoin(kubeflow_endpoint, "/pipeline"),
+        existing_token=bearer_token,
+    )
+    print(client.list_experiments())
