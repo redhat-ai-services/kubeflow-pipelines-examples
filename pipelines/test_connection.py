1a6077b1fa4d3a96ad4f7033f0d2edc2a8861eeb
Trevor Royer <troyer@redhat.com>
2023-03-14 12:36:33 -0500

    Switch to kfp_tekton for consistency

--- pipelines/test_connection.py
+++ pipelines/test_connection.py
@@ -4,7 +4,7 @@ import urllib
 
 from dotenv import load_dotenv
 
-import kfp
+import kfp_tekton
 
 load_dotenv()
 
@@ -12,7 +12,7 @@ kubeflow_endpoint = os.environ["KUBEFLOW_ENDPOINT"]
 bearer_token = os.environ["BEARER_TOKEN"]
 
 if __name__ == "__main__":
-    client = kfp.Client(
+    client = kfp_tekton.TektonClient(
         host=urllib.parse.urljoin(kubeflow_endpoint, "/pipeline"),
         existing_token=bearer_token,
     )
