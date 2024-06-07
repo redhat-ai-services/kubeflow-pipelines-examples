d24ee220cad50fd15060241e9648486891c4f671
Trevor Royer <troyer@redhat.com>
2024-06-07 13:31:51 -0600

    make route detection more dynamic

--- pipelines/11_iris_training_pipeline.py
+++ pipelines/11_iris_training_pipeline.py
@@ -232,17 +232,17 @@ if __name__ == "__main__":
     print(f"Connecting to kfp: {kubeflow_endpoint}")
 
     sa_token_path = "/run/secrets/kubernetes.io/serviceaccount/token"  # noqa: S105
-    if os.path.isfile(sa_token_path):
+    if os.environ["BEARER_TOKEN"]:
+        bearer_token = os.environ["BEARER_TOKEN"]
+    elif os.path.isfile(sa_token_path):
         with open(sa_token_path) as f:
             bearer_token = f.read().rstrip()
-    else:
-        bearer_token = os.environ["BEARER_TOKEN"]
 
     # Check if the script is running in a k8s pod
     # Get the CA from the service account if it is
     # Skip the CA if it is not
     sa_ca_cert = "/run/secrets/kubernetes.io/serviceaccount/service-ca.crt"
-    if os.path.isfile(sa_ca_cert):
+    if os.path.isfile(sa_ca_cert) and "svc" in kubeflow_endpoint:
         ssl_ca_cert = sa_ca_cert
     else:
         ssl_ca_cert = None
